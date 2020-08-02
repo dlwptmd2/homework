4b826ed4cee1494e692a0e69c461c33930fc8e0b
dlwptmd2 <67719752+dlwptmd2@users.noreply.github.com>
2020-08-03 06:07:13 +0900

    Update week_3 homework.py
    오예 성공오오오오오오오오옹

--- week_3 homework.py
+++ week_3 homework.py
@@ -18,21 +18,21 @@ songs = soup.select('#body-content > div > div > table > tbody > tr')
 # movies (tr들) 의 반복문을 돌리기
 for song in songs:
     # movie 안에 a 가 있으면,
-    a_tag = song.select_one('td.info > a')
+    a_tag = song.select_one('td.info > a.title.ellipsis')
     if a_tag is not None:
         # a의 text를 찍어본다.
-        title = a_tag.text  # a 태그 사이의 텍스트를 가져오기
+        title = a_tag.text.strip()  # a 태그 사이의 텍스트를 가져오기
         rank = song.select_one('td.number').text[0:2].strip()
         # 어떻게 오직 숫자만 출력할 수 있을까#아 저 [0:2}가 어디부터 어디까지 만 인쇄하라 그런 뜻인가 보네
         artist = song.select_one('td.info > a.artist.ellipsis').text
         #artist= song.select_one('a.artist ellipsis').text 이건 아니야 왤까 그나저나
         #뒤에 붙은 .text는 안에 있는 텍스트를 추출한다는 의미인가?
         #구글 크롬 콘솔 돌려보니 하단에 뜨는거 그대로 쓰면 되네 띄어쓰기 대신에 .요걸로 이어 놨어
-        #그래도 모양이 안이뻐 어카지 strip얼른 써볼까
+        #그래도 모양이 안이뻐 어카지 strip얼른 써볼까까 아이미적용된거네 그럼 a_tag요걸 건드리면 되는건가
+        #아니지 그걸 건드리는게 아니라 하단부에 타이틀옆에 스트립을 붙여주고 프린트에 타이틀 자체를 넣어주는 거야 오예 성공
 
 
-
-        print(rank, a_tag.text, artist)
+        print(rank, title, artist)
           # td 태그 사이의 텍스트를 가져오기
 
 
